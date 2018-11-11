67f7a18f7675b23ba2d0e1ffa4cdca9c34883c80
z-bowen <zach.bowen@gmail.com>
2018-11-11 12:30:33 +0100

    Added a projects page

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const fs = require('fs');
 const port = process.env.PORT || 3000;
 var app = express();
 
+// Maintenance toggle
+var maintenance = false
+
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 
@@ -20,9 +23,11 @@ app.use((req, res, next) => {
   next();
 });
 
-// app.use((req, res, next) => {
-//   res.render('maintenance.hbs', {})
-// })
+if (maintenance) {
+  app.use((req, res, next) => {
+    res.render('maintenance.hbs', {})
+  });
+}
 
 app.use(express.static(__dirname + '/public'));
 
@@ -37,13 +42,26 @@ app.get('/', (req, res) => {
   // res.send('<h1>Hello Express!</h1>');
   res.render('home.hbs', {
     pageTitle: 'Home',
-    welcomeMessage: 'Welcome to my home page'
+    description: 'Welcome to my home page'
   })
 });
 
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
     pageTitle: 'About Page'
+  });
+});
+
+app.get('/help', (req, res) => {
+  res.render('help.hbs', {
+    pageTitle: 'Help Page'
+  });
+});
+
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects Page',
+    description: 'Here is a link to my GitHub profile'
   });
 });
 
